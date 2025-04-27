f051bd8e2d40e3dc612672511d27428178494513
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-04-27 15:50:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const firstName = document.getElementById('name_f')
+const lastName = document.getElementById('name_l')
+const phoneNum = document.getElementById('phone')
+const email = document.getElementById('email')
+const submitBtn = document.getElementById('submit')
+
+function handleClick(){
+	alert(`${firstName.name} ${firstName.value}`)
+	alert(`${lastName.name} ${lastName.value}`)
+	alert(`${phoneNum.name} ${phoneNum.value}`)
+	alert(`${email.name} ${email.value}`)
+	
+}
+submitBtn.addEventListener('click', handleClick)
